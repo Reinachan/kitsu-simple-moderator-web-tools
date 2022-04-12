dea97874cf004b066512600820e2ac49efbb0ee3
Nina Louise <ninalouise@reinina.moe>
2022-04-12 17:31:38 +0200

    Errorhandle login

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -5,95 +5,95 @@ import { TextInput } from 'components/TextInput';
 import { Card } from 'components/Card';
 import styles from './styles.module.css';
 
-interface TokenQueryResponse {
-	access_token: string; // Token used in Authorization header
-	created_at: number;
-	expires_in: number; // Seconds until the access_token expires (30 days default)
-	refresh_token: string; // Token used to get a new access_token
-	scope: 'public';
-	token_type: 'bearer';
+interface Res {
+  access_token: string; // Token used in Authorization header
+  created_at: number;
+  expires_in: number; // Seconds until the access_token expires (30 days default)
+  refresh_token: string; // Token used to get a new access_token
+  scope: 'public';
+  token_type: 'bearer';
 }
 
-interface TokenQueryError {
-	error:
-		| 'invalid_request'
-		| 'invalid_client'
-		| 'invalid_grant'
-		| 'invalid_scope'
-		| 'unauthorized_client'
-		| 'unsupported_grant_type';
-	error_description: string;
+interface Err {
+  error:
+    | 'invalid_request'
+    | 'invalid_client'
+    | 'invalid_grant'
+    | 'invalid_scope'
+    | 'unauthorized_client'
+    | 'unsupported_grant_type';
+  error_description: string;
 }
 
 interface LoginProps {
-	setToken: React.Dispatch<React.SetStateAction<string>>;
+  setToken: React.Dispatch<React.SetStateAction<string>>;
 }
 
 export default function Login({ setToken }: LoginProps) {
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-	const [{ data, loading, error }, execute] = useAxios<TokenQueryResponse>(
-		{
-			url: 'https://kitsu.io/api/oauth/token',
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		},
-		{ manual: true }
-	);
+  const [{ data, loading, error }, execute] = useAxios<Res, any, Err>(
+    {
+      url: 'https://kitsu.io/api/oauth/token',
 
-	const onSubmit = (e: FormEvent) => {
-		e.preventDefault();
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+    { manual: true }
+  );
 
-		execute({
-			data: {
-				grant_type: 'password',
-				username: username,
-				password: password, // RFC3986 URl encoded string
-			},
-		});
-	};
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
 
-	useEffect(() => {
-		console.log('ahoy');
+    execute({
+      data: {
+        grant_type: 'password',
+        username: username,
+        password: password, // RFC3986 URl encoded string
+      },
+    });
+  };
 
-		if (data && !loading && !error) {
-			console.log('no hoy');
+  useEffect(() => {
+    if (data && !loading && !error) {
+      setToken(data.access_token);
+    }
+  }, [data, loading, error]);
 
-			setToken(data.access_token);
-		}
-	}, [data, loading, error]);
+  return (
+    <Card className={styles.card}>
+      <form onSubmit={onSubmit}>
+        <label>
+          Username
+          <TextInput
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.currentTarget.value)}
+          ></TextInput>
+        </label>
 
-	if (!loading && error) {
-		console.error(error);
-		return <p>{JSON.stringify(error)}</p>;
-	}
+        <label>
+          Password
+          <TextInput
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.currentTarget.value)}
+          ></TextInput>
+        </label>
 
-	return (
-		<Card className={styles.card}>
-			<form onSubmit={onSubmit}>
-				<label>
-					Username
-					<TextInput
-						type='text'
-						value={username}
-						onChange={(e) => setUsername(e.currentTarget.value)}></TextInput>
-				</label>
+        <Button kind={ButtonKind.PRIMARY} loading={loading}>
+          Submit
+        </Button>
+      </form>
 
-				<label>
-					Password
-					<TextInput
-						type='password'
-						value={password}
-						onChange={(e) => setPassword(e.currentTarget.value)}></TextInput>
-				</label>
-
-				<Button kind={ButtonKind.PRIMARY} loading={loading}>
-					Submit
-				</Button>
-			</form>
-		</Card>
-	);
+      {!loading && error && (
+        <>
+          <p>Wrong username or password, try again</p>
+        </>
+      )}
+    </Card>
+  );
 }
